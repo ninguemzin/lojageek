5bed979bee1a131278d9ed1c242ada5d27406b58
ninguemzin <alm1427@hotmail.com>
2024-12-04 22:39:44 -0300

    first

---
+++ src/main/java/com/generation/lojageek/LojageekApplication.java
@@ -0,0 +1,13 @@
+package com.generation.lojageek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojageekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojageekApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/generation/lojageek/model/Produto.java
@@ -0,0 +1,72 @@
+package com.generation.lojageek.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	private String plataforma;
+	
+	private int ano_lancamento;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public int getAno_lancamento() {
+		return ano_lancamento;
+	}
+
+	public void setAno_lancamento(int ano_lancamento) {
+		this.ano_lancamento = ano_lancamento;
+	}
+	
+	
+
+}
